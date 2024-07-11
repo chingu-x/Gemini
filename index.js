26930b8b0f71e0a4b0593b7fefd7961860c1bfd3
ConwayCJ <christopherjconway07@gmail.com>
2024-07-10 23:38:13 -0700

    add pingpong command and handler for commands folder

--- index.js
+++ index.js
@@ -1,9 +1,61 @@
 // Require the necessary discord.js classes
-import 'dotenv/config';
-import { Client, Events, GatewayIntentBits } from 'discord.js'
+const fs = require('node:fs');
+const path = require('node:path');
+require('dotenv').config()
 
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// New collection of commands for the client
+client.commands = new Collection()
+
+// For every folder in our 'commands' folder, go through each folder and apply each .js file
+const foldersPath = path.join(__dirname, 'commands')
+const commandFolders = fs.readdirSync(foldersPath)
+
+// Get every folder in the 'commands' folder
+for (const folder of commandFolders) {
+  // Get every .js file for each subdirectory under the 'commands' folder
+  const commandsPath = path.join(foldersPath, folder)
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file)
+    const command = require(filePath)
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command)
+    } else {
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property`)
+    }
+  }
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+  // If the interaction is not a slash command - return
+  if (!interaction.isChatInputCommand()) return;
+  console.log(interaction)
+
+  const command = interaction.client.commands.get(interaction.commandName)
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`)
+    return
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (err) {
+    console.error(err)
+    if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+    } else {
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+  }
+
+})
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
